6909bc8a5fbb06d9ebf41713068a03453527b0e1
GarionCZ <nottelling@you.com>
2015-05-30 23:41:43 +0200

    * Fixing updateURL and downloadURL links

--- sgv2-filter.user.js
+++ sgv2-filter.user.js
@@ -4,8 +4,8 @@
 // @description Giveaway filter for SteamGifts v2
 // @author      Garion
 // @include     http://www.steamgifts.com/*
-// @downloadURL https://github.com/GarionCZ/sgv2-filter/blob/master/sgv2-filter.user.js
-// @updateURL   https://github.com/GarionCZ/sgv2-filter/blob/master/sgv2-filter.meta.js
+// @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.user.js
+// @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.meta.js
 // @version     0.1-DEV
 // @grant       GM_getValue
 // @grant       GM_setValue
