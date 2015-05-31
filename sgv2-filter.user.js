9633c0df3d1b913a372ecd646ce42f7b58304314
GarionCZ <nottelling@you.com>
2015-05-31 10:34:26 +0200

    * Changing the downloadURL and updateURL
    Changing the downloadURL and updateURL to point to the beta branch.

--- sgv2-filter.user.js
+++ sgv2-filter.user.js
@@ -4,8 +4,8 @@
 // @description Giveaway filter for SteamGifts v2
 // @author      Garion
 // @include     http://www.steamgifts.com/*
-// @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.user.js
-// @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.meta.js
+// @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/beta/sgv2-filter.user.js
+// @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/beta/sgv2-filter.meta.js
 // @version     0.2-BETA
 // @grant       GM_getValue
 // @grant       GM_setValue
