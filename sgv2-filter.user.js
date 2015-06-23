6fc991c49b5c0f0ec56f56287e36d972e08ba809
GarionCZ <nottelling@you.com>
2015-06-23 17:10:11 +0200

    * Changing version to 0.4.0-DEV

--- sgv2-filter.user.js
+++ sgv2-filter.user.js
@@ -6,7 +6,7 @@
 // @include     http://www.steamgifts.com/*
 // @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.user.js
 // @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.meta.js
-// @version     0.3.0-DEV
+// @version     0.4.0-DEV
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
