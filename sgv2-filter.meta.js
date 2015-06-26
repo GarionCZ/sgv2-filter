c1d506ca3890613f3fdfc086ff05c4f970afd397
GarionCZ <nottelling@you.com>
2015-06-26 16:28:46 +0200

    * Changing version to 0.4.2-BETA

--- sgv2-filter.meta.js
+++ sgv2-filter.meta.js
@@ -6,7 +6,7 @@
 // @include     http://www.steamgifts.com/*
 // @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/beta/sgv2-filter.user.js
 // @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/beta/sgv2-filter.meta.js
-// @version     0.4.2-DEV
+// @version     0.4.2-BETA
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
