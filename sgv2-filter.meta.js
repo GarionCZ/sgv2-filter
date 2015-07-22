6619a0eba4920169ed0aa7db9a9cf15c9fd6041d
0657code <0657code@users.noreply.github.com>
2015-07-22 15:50:38 +0200

    Corrected includes
    The script is now only active on the giveaway overviews and user profiles

--- sgv2-filter.meta.js
+++ sgv2-filter.meta.js
@@ -3,7 +3,8 @@
 // @namespace   https://github.com/GarionCZ/sgv2-filter
 // @description Giveaway filter for SteamGifts v2
 // @author      Garion
-// @include     http://www.steamgifts.com/*
+// @include     http://www.steamgifts.com/giveaways*
+// @include     http://www.steamgifts.com/user/*
 // @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.user.js
 // @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.meta.js
 // @version     0.4.2-DEV
