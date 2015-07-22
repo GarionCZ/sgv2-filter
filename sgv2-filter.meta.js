f24e9319472084eb71e329459c5bc331f1f8fff6
0657code <0657code@users.noreply.github.com>
2015-07-22 15:50:54 +0200

    Update sgv2-filter.meta.js

--- sgv2-filter.meta.js
+++ sgv2-filter.meta.js
@@ -7,7 +7,7 @@
 // @include     http://www.steamgifts.com/user/*
 // @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.user.js
 // @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.meta.js
-// @version     0.4.2-DEV
+// @version     0.4.3-DEV
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
