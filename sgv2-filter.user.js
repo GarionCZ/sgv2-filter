4af1ba62561388c9ab358901707702ef6d89814d
GarionCZ <nottelling@you.com>
2016-02-03 23:51:49 +0100

    Cherry-picked: Merge pull request #2 from 0657code/master
    * Corrects includes to not show the filter in the giveaway detail page

--- sgv2-filter.user.js
+++ sgv2-filter.user.js
@@ -3,7 +3,9 @@
 // @namespace   https://github.com/GarionCZ/sgv2-filter
 // @description Giveaway filter for SteamGifts v2
 // @author      Garion
-// @include     http://www.steamgifts.com/*
+// @include     http://www.steamgifts.com/
+// @include     http://www.steamgifts.com/giveaways*
+// @include     http://www.steamgifts.com/user/*
 // @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/release/sgv2-filter.user.js
 // @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/release/sgv2-filter.meta.js
 // @version     0.4.3-BETA
