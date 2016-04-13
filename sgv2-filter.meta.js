e9e6376d01690dce7d19de9c07bceda7cb93f0cb
GarionCZ <nottelling@you.com>
2016-04-13 16:19:25 +0200

    * Fixes version in sgv2-filter.meta.js

--- sgv2-filter.meta.js
+++ sgv2-filter.meta.js
@@ -8,7 +8,7 @@
 // @include     http://www.steamgifts.com/user/*
 // @downloadURL https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.user.js
 // @updateURL   https://github.com/GarionCZ/sgv2-filter/raw/master/sgv2-filter.meta.js
-// @version     0.5.4-DEV
+// @version     0.5.5-DEV
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
